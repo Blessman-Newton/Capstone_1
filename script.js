7e403b31c76b0e6bb289d52069803bdbfb58729b
Blessman-Newton <blessmannewton0@gmail.com>
2023-05-31 03:51:14 +0100

    modify the program array

--- script.js
+++ script.js
@@ -1,113 +1,113 @@
 const program = [
-    {
-        icon:'images/icon1.png',
-        title:'Networking',
-        content:`   
+  {
+    icon: 'images/icon1.png',
+    title: 'Networking',
+    content: `   
         Connect with industry experts, 
         fellow data scientists, 
         and AI enthusiasts..
              `,
-    },{
-        icon:'images/icon2.png',
-        title:'Insights',
-        content:`
+  }, {
+    icon: 'images/icon2.png',
+    title: 'Insights',
+    content: `
         Gain valuable insights 
         from keynote speakers 
         and panel discussions..
          `,
-    },{
-        icon:'images/icon3.png',
-        title:'Workshops',
-        content:`
+  }, {
+    icon: 'images/icon3.png',
+    title: 'Workshops',
+    content: `
         Participate in hands-on workshops to 
         sharpen your data science and AI skills.
          `,
-    },{
-        icon:'images/program_icon_04.png',
-        title:'Workshop',
-        content:`  
+  }, {
+    icon: 'images/program_icon_04.png',
+    title: 'Workshop',
+    content: `  
             Try creating your own work using open 
             source licence rather than watching at it.
         `,
-    },{
-        icon:'images/program_icon_05.png',
-        title:'Network',
-        content:`
+  }, {
+    icon: 'images/program_icon_05.png',
+    title: 'Network',
+    content: `
         Get opportunities to 
         network with students 
         and developers around the world.
         `,
-    },
+  },
 ];
 
 const features = [
-    {
-        icon:'images/now.jpg',
-        name:'Jane Smith',
-        rank: 'Data Scientist',
-        star: 'DataCamp',
-        content:`   
+  {
+    icon: 'images/now.jpg',
+    name: 'Jane Smith',
+    rank: 'Data Scientist',
+    star: 'DataCamp',
+    content: `   
         With 10 years of experience in the 
         field, Jane has worked with leading
          tech companies and has been instrumental
          in developing advanced AI solutions.
              `,
-    },
-    {
-        icon:'images/now.jpg',
-        name:'John Davis',
-        rank: 'AI researcher',
-        star: 'ALX',
-        content:`   
+  },
+  {
+    icon: 'images/now.jpg',
+    name: 'John Davis',
+    rank: 'AI researcher',
+    star: 'ALX',
+    content: `   
         John has dedicated his career 
         to pushing the
          boundaries of AI and machine learning.
              `,
-    },{
-        icon:'images/now.jpg',
-        name:'Maria Garcia',
-        rank: 'Data Science Consultant',
-        star: 'NomPass',
-        content:`   
+  }, {
+    icon: 'images/now.jpg',
+    name: 'Maria Garcia',
+    rank: 'Data Science Consultant',
+    star: 'NomPass',
+    content: `   
         With a background in statistics and 
         data analysis, Maria has helped 
         organizations across diverse sectors
          leverage their
          data for strategic decision-making.
              `,
-    },{
-        icon:'images/now.jpg',
-        name:'David Johnson',
-        rank: 'Entrepreneur',
-        star: 'ExpressWord',
-        content:`   
+  }, {
+    icon: 'images/now.jpg',
+    name: 'David Johnson',
+    rank: 'Entrepreneur',
+    star: 'ExpressWord',
+    content: `   
         David Johnson is a seasoned entrepreneur and AI strategist.
         With a successful track record of launching AI-driven startups.
              `,
-    },{
-        icon:'images/now.jpg',
-        name:'Blessman Newton',
-        rank: 'Full Stack Developer',
-        star: 'Microverse',
-        content:`   
+  }, {
+    icon: 'images/now.jpg',
+    name: 'Blessman Newton',
+    rank: 'Full Stack Developer',
+    star: 'Microverse',
+    content: `   
         Blessman is passionate about bridging the gap between
          technical expertise and business outcomes, and her
           presentations focus on practical
          applications of data science in real-world scenarios.
              `,
-    },{
-        icon:'images/now.jpg',
-        name:'Caleb Anaane',
-        rank: 'Student',
-        star: 'DataCamp',
-        content:`   
+  }, {
+    icon: 'images/now.jpg',
+    name: 'Caleb Anaane',
+    rank: 'Student',
+    star: 'DataCamp',
+    content: `   
         David is a dynamic speaker who combines 
         his business acumen with technical knowledge
          to inspire and educate
          audiences on the potential of AI
           for innovation and disruption..
              `,
-    },
+  },
 ];
 
 const hamburger = document.querySelector('.hamburger');
@@ -117,22 +117,20 @@ const backMenu = document.querySelectorAll('.nav-list');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   Menu.classList.toggle('active');
-  console.log('Thank')
 });
 
 backMenu.forEach((element) => element.addEventListener('click', () => {
   hamburger.classList.remove('active');
   Menu.classList.remove('active');
-  console.log('Bye!')
 }));
 
 document.addEventListener('DOMContentLoaded', () => {
-const programs = document.querySelector('.event');
-const feature = document.querySelector('.recent');
+  const programs = document.querySelector('.event');
+  const feature = document.querySelector('.recent');
 
-let cardTemplate = '';
-let featureTemplate = '';
-program.forEach((card) => {
+  let cardTemplate = '';
+  let featureTemplate = '';
+  program.forEach((card) => {
     const tag = `
             
                 <div class="programs">
@@ -144,10 +142,10 @@ program.forEach((card) => {
                 </div>
     `;
     cardTemplate += tag;
-});
-programs.innerHTML = cardTemplate;
+  });
+  programs.innerHTML = cardTemplate;
 
-features.forEach((card) => {
+  features.forEach((card) => {
     const tag = `
          <div class="featured-card">
                         <div class="img-bg">
@@ -164,6 +162,6 @@ features.forEach((card) => {
                     </div>
     `;
     featureTemplate += tag;
-});
-feature.innerHTML = featureTemplate;
+  });
+  feature.innerHTML = featureTemplate;
 });
